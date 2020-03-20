c20fff057d48009b9d25c11a41734285117d66d0
Joseph H Kennedy <me@jhkennedy.org>
2020-03-19 20:35:29 -0800

    Add pre-gen hook for project and package name validation and missing year variable

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+PROJECT_REGEX = r'^[_a-zA-Z][\-_a-zA-Z0-9]+$'
+PACKAGE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+project_name = '{{ cookiecutter.project_name }}'
+package_name = '{{ cookiecutter.package_name }}'
+
+if not re.match(PROJECT_REGEX, project_name):
+    print(f'ERROR: {project_name} is not a valid Gitlab project name!\n'
+          f'  Name should match this regex: {PROJECT_REGEX}')
+    sys.exit(1)
+
+if not re.match(PACKAGE_REGEX, package_name):
+    print(f'ERROR: {package_name} is not a valid Python package name!\n'
+          f'  See: https://pep8.org/#package-and-module-names')
+    sys.exit(1)
