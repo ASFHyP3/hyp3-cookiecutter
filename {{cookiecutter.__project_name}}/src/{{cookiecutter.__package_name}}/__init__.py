9ee65b8d0e0d86cd48ead406befc8211f004902e
Jake Herrmann <jtherrmann@alaska.edu>
2023-03-24 10:40:31 -0800

    newline between import groups

--- {{cookiecutter.__project_name}}/src/{{cookiecutter.__package_name}}/__init__.py
+++ {{cookiecutter.__project_name}}/src/{{cookiecutter.__package_name}}/__init__.py
@@ -1,6 +1,7 @@
 """{{cookiecutter.short_description}}"""
 
 from importlib.metadata import version
+
 from {{cookiecutter.__package_name}}.process import {{cookiecutter.__process_name}}
 
 __version__ = version(__name__)
